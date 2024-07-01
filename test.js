666cae73444b4e295118e0419840655c9549780b
Satyam Banwale <satyambanwale@prodios.com>
2024-07-01 18:03:03 +0530

    testing if conditions

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 if (0) {
-    console.log('sahi hai')
-  }
-  else {
-    console.log('galat hai')
+  console.log("sahi hai");
+} else {
+  console.log("galat hai");
+}
+
+if ("Satyam") {
+  console.log("sahi hai");
+} else {
+  console.log("galat hai");
+}
+
+
+if (5 == -5) {
+    console.log("sahi hai");
+  } else {
+    console.log("galat hai");
   }
